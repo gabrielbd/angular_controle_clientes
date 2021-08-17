88cb005f565a45726fe19ac48be40c9cf6d550a8
Gabriel Bronisio <gabrieldbronisio@gmail.com>
2021-08-17 20:51:36 -0300

    finish

--- src/app/relatorio-clientes/relatorio-clientes.component.ts
+++ src/app/relatorio-clientes/relatorio-clientes.component.ts
@@ -1,32 +1,64 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-relatorio-clientes',
-  templateUrl: './relatorio-clientes.component.html',
-  styleUrls: ['./relatorio-clientes.component.css']
+  selector: 'app-relatorio-clientes',
+  templateUrl: './relatorio-clientes.component.html',
+  styleUrls: ['./relatorio-clientes.component.css']
 })
-export class RelatorioClientesComponent implements OnInit {
+export class RelatorioClientesComponent implements OnInit {
 
-  constructor() { }
+  //inicializando a classe HttpClient (injeção de dependencia)
+  constructor(private httpClient: HttpClient) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {
+  }
 
-  formRelatorio = new FormGroup({
-    dataMin: new FormControl('', [
-      Validators.required
-    ]),
-    dataMax: new FormControl('', [
-      Validators.required
-    ])
-  });
+  formRelatorio = new FormGroup({
+    dataMin: new FormControl('', [
+      Validators.required
+    ]),
+    dataMax: new FormControl('', [
+      Validators.required
+    ])
+  });
 
-  get form(): any {
-    return this.formRelatorio.controls;
-  }
+  get form(): any {
+    return this.formRelatorio.controls;
+  }
 
-  onSubmit(): void {
-    console.log(this.formRelatorio.value);
-  }
+  //função executada pelo SUBMIT do formulário
+  onSubmit(): void {
+
+    //capturando as datas selecionadas no formulário
+    var dataMin = this.formRelatorio.value.dataMin;
+    var dataMax = this.formRelatorio.value.dataMax;
+
+    //executando a chamada para a API..
+    this.httpClient
+      .get('http://localhost:3217/api/Clientes/Relatorio/' 
+          + dataMin + "/" + dataMax,
+        { responseType: 'arraybuffer' })
+      .subscribe( //capturar o retorno da API
+        (success) => { //retorno de sucesso
+          
+          var arquivo = new Blob([success], { type : 'application/pdf'});
+          var nomeArquivo = "relatorio_clientes.pdf";
+
+          //DOWNLOAD..
+          var url = window.URL.createObjectURL(arquivo);
+          var downloadFile = document.createElement('a');
+          downloadFile.href = url;
+          downloadFile.download = nomeArquivo;
+
+          document.body.appendChild(downloadFile);
+          downloadFile.click();
+          document.body.removeChild(downloadFile);
+        },
+        (error) => { //retorno de erro
+          console.log(error);
+        }
+      )
+  }
 }
